e7f290befc332f5bc4964f486e6bebd3138c6f0a
Francis Avila <favila@breezeehr.com>
2019-03-15 18:39:57 -0500

    Add clojure dep

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/Breezeemr/hitch2/tree/master/"
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
                  ["releases" {:url "s3p://breezepackages/releases" :creds :gpg}]]
-  :dependencies [[com.cognitect/transit-clj "0.8.313"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.cognitect/transit-clj "0.8.313"]
                  [com.breezeehr/hitch2 "0.3.2-SNAPSHOT"
                   :exclusions [com.clojure/clojurescript]]])
