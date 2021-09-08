b832e8dec98aa947b94e8be04abcc2fb1915278b
Tyler Tallman <dspiteself@gmail.com>
2021-09-08 11:12:58 -0500

    add explicit transit encoders

--- src/hitch2/transit/basic.clj
+++ src/hitch2/transit/basic.clj
@@ -36,3 +36,13 @@
   ([^OutputStream out type] (writer out type {}))
   ([^OutputStream out type opts]
     (t/writer out type (update opts :handlers (fnil assoc {}) Descriptor  DTORHandler) )))
+
+(defn make-string-reader [ type opts]
+  (let [opts (update opts :handlers (fnil assoc {}) "DTOR" make-pos-dtor)]
+    (fn [^InputStream in]
+      (t/read (t/reader in type opts)))))
+
+(defn make-string-writer [ type opts]
+  (let [opts (update opts :handlers (fnil assoc {}) Descriptor  DTORHandler) ]
+    (fn [^OutputStream out]
+      (t/writer out type opts))))
